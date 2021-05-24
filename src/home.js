a4e1d65472db0990e6a67354a979b508fdfb3f67
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2021-05-24 02:35:47 -0700

    chore: finalizar páginas con nuevo contenido estructurado con html puro

--- src/home.js
+++ src/home.js
@@ -1,38 +1,38 @@
 $(document).ready(function () {
     $.getJSON('../Menu.json', function(data) {
-        console.log(data)
         for(var i in data.menu)
         { 
-            $(".main-menu").append(`<button id="${data.menu[i].id}" class="nav-buttons"><li class="nav-items"><p>${data.menu[i].nombre}</p></li></button>`)
+            $(".main-menu").append(`<button id="${data.menu[i].id}" class="nav-buttons clickeable"><li class="nav-items clickeable"><p id="${data.menu[i].id}" class="clickeable">${data.menu[i].nombre}</p></li></button>`)
         }
-        clickevents();
+        loadClickEvent()
     })
 })
 
-function clickevents() {
-    $("#home").unbind();
-    $("#home").click(e =>{
-        $("#menu-content").load("../pages/home.html")
-    })
+function loadContentPages(pageName) {
+    return fetch(pageName).then(data => data.text()).then(data => data)
+}
 
-    $("#music").unbind();
-    $("#music").click(e =>{
-        console.log("cargando")
-        $("#menu-content").load("../pages/music.html")
-    })
+function loadClickEvent(){
+    let button = document.querySelectorAll('.clickeable');
+    for (let field of Array.from(button))
+        field.addEventListener("click", event => {
+            event.stopImmediatePropagation()
+            loadPages(event.target.id);
+        })
+}
 
-    $("#sheets").unbind();
-    $("#sheets").click(e =>{
-        $("#menu-content").load("../pages/partituras.html")
-    })
-
-    $("#calender").unbind();
-    $("#calender").click(e =>{
-        $("#menu-content").load("../pages/calendario.html")
-    })
-
-    $("#contact").unbind();
-    $("#contact").click(e =>{
-        $("#menu-content").load("../pages/contacto.html")
-    })
+function loadPages(id){
+    console.log(id)
+    switch(id){
+        case 'home': return loadContentPages("../pages/home.html").then(content => {
+            document.getElementById('menu-content').innerHTML = content}) ;
+        case 'music': return loadContentPages("../pages/music.html").then(content => {
+            document.getElementById('menu-content').innerHTML = content}) ;
+        case 'sheets': return loadContentPages("../pages/partituras.html").then(content => {
+            document.getElementById('menu-content').innerHTML = content}) ;
+        case 'calender': return loadContentPages("../pages/calendario.html").then(content => {
+            document.getElementById('menu-content').innerHTML = content}) ;
+        case 'contact': return loadContentPages("../pages/contacto.html").then(content => {
+            document.getElementById('menu-content').innerHTML = content}) ;
+    }
 }
